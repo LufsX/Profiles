47bb6ff405c698db25abbf2d02bdcb1fb17a7628
LufsX <Lufs00@outlook.com>
2022-07-27 20:10:42 +0800

    chore: new tool

---
+++ Tools/surge2clash.py
@@ -0,0 +1,20 @@
+import os
+
+for dirpath, dirnames, filenames in os.walk('../List/Surge'):
+    for filename in filenames:
+        print(os.path.join(dirpath, filename))
+        with open(os.path.join(dirpath, filename),"rt") as content:
+            text = content.readlines()
+            filePath = '../List/Clash/'+filename.replace("conf","yaml")
+            if os.path.exists(filePath):
+                os.remove(filePath)
+            f = open('../List/Clash/'+filename.replace("conf","yaml"),"a")
+            f.writelines("payload:\n")
+            for line in text:
+                if line.strip()=="":
+                    f.writelines("")
+                elif line.startswith('#'):
+                    line = '  '+line
+                else:
+                    line = '  - '+line
+                f.writelines(line)
