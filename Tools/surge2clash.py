b561fb426ca35a746dfb44eece224776aeedd1c2
LufsX <Lufs00@outlook.com>
2022-08-08 17:21:20 +0800

    feat: guard list

--- Tools/surge2clash.py
+++ Tools/surge2clash.py
@@ -11,12 +11,19 @@ for dirpath, dirnames, filenames in os.walk('../List/Surge'):
             f = open('../List/Clash/'+filename.replace("conf","yaml"),"a")
             f.writelines("payload:\n")
             for line in text:
+                # 处理空行
                 if line.strip()=="":
                     f.writelines("")
+                # 处理注释
                 elif line.startswith('#'):
                     line = '  '+line
+                # 处理 Clash 不支持的规则
                 elif line.startswith('USER-AGENT'):
                     line = '  # - '+line
+                # 处理 Domainset
+                elif line.startswith('.'):
+                    line = '  - *'+line
+                # 处理正常规则
                 else:
                     line = '  - '+line
                 f.writelines(line)
