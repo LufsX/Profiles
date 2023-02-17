6230bd144e34aeaf78e6589fe13670f2d1810a40
LufsX <lufsx@outlook.com>
2023-02-17 19:15:00 +0800

    tool: can now be executed anywhere

--- Tools/clash2surge.py
+++ Tools/clash2surge.py
@@ -1,9 +1,29 @@
-import os
+import os, sys
 
-for dirpath, dirnames, filenames in os.walk('../List/Clash'):
+surgeRulesetDir = os.path.join(os.path.abspath(os.path.dirname(sys.path[0])),
+                               'List', 'Surge') + '/'
+clashRulesetDir = os.path.join(os.path.abspath(os.path.dirname(sys.path[0])),
+                               'List', 'Clash') + '/'
+
+for dirpath, dirnames, filenames in os.walk(clashRulesetDir):
     for filename in filenames:
         print(os.path.join(dirpath, filename))
-        with open(os.path.join(dirpath, filename),"rt") as content:
-            text = content.read()
-            text = text.replace("payload:\n","").replace("  ","").replace("- ","").replace("# USER-AGENT","USER-AGENT")
-            open('../List/Surge/'+filename.replace("yaml","conf"),"w").write(text)
+        with open(os.path.join(dirpath, filename), "rt") as content:
+            text = content.readlines()
+            filePath = surgeRulesetDir + filename.replace("yaml", "conf")
+            if os.path.exists(filePath):
+                os.remove(filePath)
+            f = open(surgeRulesetDir + filename.replace("yaml", "conf"), "a")
+            for line in text:
+                if line == "payload:":
+                    break
+                # 处理 Surge 支持而 Clash 不支持的规则
+                elif line.startswith("  # - USER-AGENT"):
+                    f.writelines(line.replace("  # - ", ""))
+                elif line.startswith("  - "):
+                    f.writelines(line.replace("  - ", ""))
+                elif line.startswith("  #"):
+                    f.writelines(line.replace("  #", "#"))
+                # 处理空行
+                elif line == "\n":
+                    f.writelines("\n")
--- Tools/surge2clash.py
+++ Tools/surge2clash.py
@@ -1,29 +1,34 @@
-import os
+import os, sys
 
-for dirpath, dirnames, filenames in os.walk('../List/Surge'):
+surgeRulesetDir = os.path.join(os.path.abspath(os.path.dirname(sys.path[0])),
+                               'List', 'Surge') + '/'
+clashRulesetDir = os.path.join(os.path.abspath(os.path.dirname(sys.path[0])),
+                               'List', 'Clash') + '/'
+
+for dirpath, dirnames, filenames in os.walk(surgeRulesetDir):
     for filename in filenames:
         print(os.path.join(dirpath, filename))
-        with open(os.path.join(dirpath, filename),"rt") as content:
+        with open(os.path.join(dirpath, filename), "rt") as content:
             text = content.readlines()
-            filePath = '../List/Clash/'+filename.replace("conf","yaml")
+            filePath = clashRulesetDir + filename.replace("conf", "yaml")
             if os.path.exists(filePath):
                 os.remove(filePath)
-            f = open('../List/Clash/'+filename.replace("conf","yaml"),"a")
+            f = open(clashRulesetDir + filename.replace("conf", "yaml"), "a")
             f.writelines("payload:\n")
             for line in text:
                 # 处理空行
-                if line.strip()=="":
+                if line.strip() == "":
                     f.writelines("")
                 # 处理注释
                 elif line.startswith('#'):
-                    line = '  '+line
+                    line = '  ' + line
                 # 处理 Clash 不支持的规则
                 elif line.startswith('USER-AGENT'):
-                    line = '  # - '+line
+                    line = '  # - ' + line
                 # # 处理 Domainset
                 # elif line.startswith('.'):
                 #     line = '  - *'+line
                 # 处理正常规则
                 else:
-                    line = '  - '+line
+                    line = '  - ' + line
                 f.writelines(line)
