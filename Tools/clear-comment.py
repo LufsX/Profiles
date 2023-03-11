59865ad75bc577773a48a2867f9051f79fcce109
LufsX <lufsx@outlook.com>
2023-03-11 18:10:08 +0800

    tool: can now be executed anywhere

--- Tools/clear-comment.py
+++ Tools/clear-comment.py
@@ -1,10 +1,17 @@
-import os
+import os, sys
+
+processDir = os.path.join(os.path.abspath(os.path.dirname(sys.path[0])), 'Config') + '/'
 
 dict = {
-"../Config/clash.yaml":"../Config/clash-nocomment.yaml",
-"../Config/surge.conf":"../Config/surge-nocomment.conf"
+    "clash.yaml": "clash-nocomment.yaml",
+    "surge.conf": "surge-nocomment.conf",
 }
 
-for a,b in dict.items():
-    text = os.popen("cat {} | grep -o '^[^#]*' | sed '/^[ ]*$/d' | sed 's/ *$//'".format(a),"r")
-    open(b,"w").write(text.read())
+for a, b in dict.items():
+    text = os.popen(
+        "cat {} | grep -o '^[^#]*' | sed '/^[ ]*$/d' | sed 's/ *$//'".format(
+            processDir + a
+        ),
+        "r",
+    )
+    open(processDir + b, "w").write(text.read())
