19bda22b7946c8940f44bbe102b72e64fdfb551b
LufsX <lufsx@outlook.com>
2023-08-09 08:56:32 +0800

    feat: photoshop express module

---
+++ Script/photoshop.js
@@ -0,0 +1,5 @@
+let obj = JSON.parse($response.body);
+obj["mobileProfile"]["profileStatus"] = "PROFILE_AVAILABLE";
+obj["mobileProfile"]["legacyProfile"] = "{}";
+obj["mobileProfile"]["relationshipProfile"] = "[]";
+$done({ body: JSON.stringify(obj) });
