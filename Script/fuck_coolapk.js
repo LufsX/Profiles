c86d81b19e2e365a0ba3fd3f160055ea73eff290
LufsX <lufsx@outlook.com>
2023-08-23 05:51:17 +0800

    chore: misc

--- Script/fuck_coolapk.js
+++ Script/fuck_coolapk.js
@@ -2,64 +2,28 @@ var requestUrl = $request.url;
 var body = $response.body;
 var obj = JSON.parse(body);
 
+const replyListRegex = /^https:\/\/api\.coolapk\.com\/v6\/feed\/replyList/;
+const detailRegex = /^https:\/\/api\.coolapk\.com\/v6\/feed\/detail/;
+const indexRegex = /^https:\/\/api\.coolapk\.com\/v6\/main\/indexV8/;
+const dataListRegex = /^https:\/\/api\.coolapk\.com\/v6\/page\/dataList/;
+const loadConfigRegex = /^https:\/\/api\.coolapk\.com\/v6\/account\/loadConfig/;
+const initRegex = /^https:\/\/api\.coolapk\.com\/v6\/main\/init/;
+const jdItemRegex = /item\.m\.jd/;
+const startupPageRegex = /启动页/;
+
 try {
-  if (/^https:\/\/api\.coolapk\.com\/v6\/feed\/replyList/.test(requestUrl)) {
+  if (replyListRegex.test(requestUrl)) {
     obj.data = obj.data.filter((item) => item.id);
-  } else if (/^https:\/\/api\.coolapk\.com\/v6\/feed\/detail/.test(requestUrl)) {
-    obj.data.include_goods = [];
-    obj.data.detailSponsorCard = [];
-    obj.data.extra_title = "";
-    obj.data.extra_pic = "";
-    obj.data.extra_key = "";
-    obj.data.extra_type = "";
-    obj.data.extra_url = "";
-    obj.data.extra_info = "";
-    obj.data.extra_fromApi = "";
-    delete obj.data.goodsListInfo;
-    obj.data.extra_entities = [];
-  } else if (/^https:\/\/api\.coolapk\.com\/v6\/main\/indexV8/.test(requestUrl)) {
-    obj.data = obj.data.filter((item) => {
-      if (item.entityTemplate === "imageCarouselCard_1" && item.entities) {
-        item.entities = item.entities.filter((el) => (el.title !== "jd" && el.title.includes("今日酷安")) || el.title.includes("众测"));
-      } else if (item.entityTemplate === "listCard") {
-        return false;
-      } else if (item.extra_title !== "") {
-        item.extra_title = "";
-        item.extra_pic = "";
-        item.extra_key = "";
-        item.extra_type = "";
-        item.extra_url = "";
-        item.extra_info = "";
-        item.extra_entities = [];
-        item.extra_fromApi = "";
-        delete item.goodsListInfo;
-      }
-      return true;
-    });
-  } else if (/^https:\/\/api\.coolapk\.com\/v6\/page\/dataList/.test(requestUrl)) {
-    obj.data.forEach((el) => {
-      el.extra_title = "";
-      el.extra_pic = "";
-      el.extra_key = "";
-      el.extra_type = "";
-      el.extra_url = "";
-      el.extra_info = "";
-      el.extra_entities = [];
-      el.extra_fromApi = "";
-      delete el.goodsListInfo;
-    });
-  } else if (/^https:\/\/api\.coolapk\.com\/v6\/account\/loadConfig/.test(requestUrl)) {
+  } else if (detailRegex.test(requestUrl)) {
+    clearDetailFields(obj.data);
+  } else if (indexRegex.test(requestUrl)) {
+    obj.data = obj.data.filter((item) => filterIndexData(item));
+  } else if (dataListRegex.test(requestUrl)) {
+    obj.data.forEach((el) => clearDetailFields(el));
+  } else if (loadConfigRegex.test(requestUrl)) {
     obj.data[0].entities = [];
-  } else if (/^https:\/\/api\.coolapk\.com\/v6\/main\/init/.test(requestUrl)) {
-    let list = obj.data.filter((el) => el.url.indexOf("item.m.jd") === -1 || el.title.indexOf("启动页") === -1);
-    list.forEach((el) => {
-      if (el.entityTemplate === "configCard") {
-        let objTem = { cardId: el.extraDataArr.cardId, cardPageName: el.extraDataArr.cardPageName, selectedHomeTab: el.extraDataArr.selectedHomeTab };
-        el.extraDataArr = objTem;
-        el.extraData = JSON.stringify(objTem);
-      }
-    });
-    obj.data = list;
+  } else if (initRegex.test(requestUrl)) {
+    optimizeInitData(obj.data);
   }
 } catch (e) {
   console.log(e);
@@ -67,3 +31,40 @@ try {
 
 body = JSON.stringify(obj);
 $done({ body });
+
+function clearDetailFields(data) {
+  data.include_goods = [];
+  data.detailSponsorCard = [];
+  data.extra_title = "";
+  data.extra_pic = "";
+  data.extra_key = "";
+  data.extra_type = "";
+  data.extra_url = "";
+  data.extra_info = "";
+  data.extra_entities = [];
+  data.extra_fromApi = "";
+  delete data.goodsListInfo;
+}
+
+function filterIndexData(item) {
+  if (item.entityTemplate === "imageCarouselCard_1" && item.entities) {
+    item.entities = item.entities.filter((el) => (el.title !== "jd" && el.title.includes("今日酷安")) || el.title.includes("众测"));
+  } else if (item.entityTemplate === "listCard") {
+    return false;
+  } else if (item.extra_title !== "") {
+    clearDetailFields(item);
+  }
+  return true;
+}
+
+function optimizeInitData(data) {
+  const filteredList = data.filter((el) => el.url.indexOf("item.m.jd") === -1 || el.title.indexOf("启动页") === -1);
+  filteredList.forEach((el) => {
+    if (el.entityTemplate === "configCard") {
+      const objTem = { cardId: el.extraDataArr.cardId, cardPageName: el.extraDataArr.cardPageName, selectedHomeTab: el.extraDataArr.selectedHomeTab };
+      el.extraDataArr = objTem;
+      el.extraData = JSON.stringify(objTem);
+    }
+  });
+  data = filteredList;
+}
--- Script/pixiv_premium.js
+++ Script/pixiv_premium.js
@@ -11,4 +11,4 @@ if (body?.user) {
 }
 body = JSON.stringify(body);
 
-$done({ body })
+$done({ body });
