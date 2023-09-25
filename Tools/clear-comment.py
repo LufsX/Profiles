b71bba708927999d6e06a80e0e6b4e96e7bc2454
LufsX <lufsx@outlook.com>
2023-09-25 12:21:43 +0800

    chore: delete replica
    and update

--- Tools/clear-comment.py
+++ Tools/clear-comment.py
@@ -1,17 +1,25 @@
-import os, sys
+import os, sys, re
 
-processDir = os.path.join(os.path.abspath(os.path.dirname(sys.path[0])), 'Config') + '/'
+processDir = os.path.join(os.path.abspath(os.path.dirname(sys.path[0])), "Config")
 
-dict = {
+file_mappings = {
     "clash.yaml": "clash-nocomment.yaml",
     "surge.conf": "surge-nocomment.conf",
 }
 
-for a, b in dict.items():
-    text = os.popen(
-        "cat {} | grep -o '^[^#]*' | sed '/^[ ]*$/d' | sed 's/ *$//'".format(
-            processDir + a
-        ),
-        "r",
-    )
-    open(processDir + b, "w").write(text.read())
+for src_file, dest_file in file_mappings.items():
+    with open(os.path.join(processDir, src_file), "r", encoding="utf-8") as src:
+        lines = src.readlines()
+
+    cleaned_lines = []
+
+    for line in lines:
+        match = re.match(r"^[^#]*", line)
+        if match:
+            result = match.group(0)
+            result = result.rstrip()
+            if result:
+                cleaned_lines.append(result + "\n")
+
+    with open(os.path.join(processDir, dest_file), "w", encoding="utf-8") as dest:
+        dest.writelines(cleaned_lines)
