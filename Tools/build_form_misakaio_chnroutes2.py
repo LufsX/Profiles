3ea4f71bff23780008348912f8629baafd6915ed
LufsX <lufsx@outlook.com>
2023-11-03 14:42:52 +0800

    fix: skip blank line

--- Tools/build_form_misakaio_chnroutes2.py
+++ Tools/build_form_misakaio_chnroutes2.py
@@ -3,7 +3,7 @@ import requests, os
 
 def build(misakaio_chnroutes2, out_dir):
     content = requests.get(misakaio_chnroutes2).text
-    exclude = ["223.118.0.0/15", "223.120.0.0/15"]
+    exclude = ["223.118.0.0/15", "223.120.0.0/15", ""]
     lines = []
     for line in content.split("\n"):
         if not line.startswith("#") and not line in exclude:
