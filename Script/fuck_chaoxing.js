e2b7090a25d4efbe4b6a40f561f2a01cff07f20f
LufsX <lufsx@outlook.com>
2024-04-08 19:17:57 +0800

    update: fuck_chaoxing.js
    同步模块的脚本更新

--- Script/fuck_chaoxing.js
+++ Script/fuck_chaoxing.js
@@ -3,12 +3,15 @@ var obj = JSON.parse($response.body);
 
 const splashRegex = /^https:\/\/learn\.chaoxing\.com\/apis\/service\/appConfig/;
 const bannerRegex = /^https:\/\/home-yd\.chaoxing\.com\/apis\/banner\/getConfigInfoForClient/;
+const appsRegex = /^https:\/\/apps\.chaoxing\.com\/apis\/recent\/getRecord\.jspx/;
 
 try {
   if (bannerRegex.test(requestUrl)) {
     obj["data"] = [];
   } else if (splashRegex.test(requestUrl)) {
     obj["data"]["ad"] = [];
+  } else if (appsRegex.test(requestUrl)) {
+    obj["list"] = obj["list"].filter(item => item.content.resTitle !== "最美图书馆摄影大赛");
   }
 } catch (e) {
   console.log(e);
