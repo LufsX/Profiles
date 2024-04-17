10c968a2661913b3bf6341f6a6225ca02c37ecba
LufsX <lufsx@outlook.com>
2024-04-17 22:07:40 +0800

    update: script

--- Script/fuck_coolapk.js
+++ Script/fuck_coolapk.js
@@ -6,24 +6,27 @@ switch (true) {
     obj.data = obj.data.filter((t) => t.id);
     break;
   case url.includes("main/init"):
-    obj.data = obj.data.filter((t) => !(945 == t.entityId));
+    obj.data = obj.data.filter((t) => ![944, 945].includes(t.entityId));
     break;
   case url.includes("indexV8"):
-    obj.data = obj.data.filter((t) => !["sponsorCard", 8639, 29349, 33006, 32557].includes(t.entityId) && !t.title.includes("值得买") && !t.title.includes("红包"));
+    obj.data = obj.data.filter((t) => !["sponsorCard", 8639, 33006].includes(t.entityId) && !t.title.includes("值得买") && !t.title.includes("红包"));
     break;
   case url.includes("dataList"):
     obj.data = obj.data.filter((t) => !("sponsorCard" == t.entityTemplate || t.title.includes("精选配件")));
     break;
   case url.includes("detail"):
-    if (obj.data?.hotReplyRows) {
-      obj.data.hotReplyRows = obj.data.hotReplyRows.filter((t) => t.id);
+    if (obj.data?.hotReplyRows?.length > 0) {
+      obj.data.hotReplyRows = obj.data.hotReplyRows.filter((item) => item?.id);
     }
-    if (obj.data?.topReplyRows) {
-      obj.data.topReplyRows = obj.data.topReplyRows.filter((t) => t.id);
+    if (obj.data?.topReplyRows?.length > 0) {
+      obj.data.topReplyRows = obj.data.topReplyRows.filter((item) => item?.id);
     }
-    obj.data.include_goods_ids = [];
-    obj.data.include_goods = [];
-    obj.data.detailSponsorCard = [];
+    const item = ["detailSponsorCard", "include_goods", "include_goods_ids"];
+    for (let i of item) {
+      if (obj.data?.[i]) {
+        obj.data[i] = [];
+      }
+    }
     break;
   default:
     break;
